1937150aab298c88d69183cd19114adb97ab4a0a
Brian-Nugent <142626899+Brian-Nugent@users.noreply.github.com>
2023-10-05 15:46:35 -0700

    Time: 1 ms (79.85%) | Memory: 40 MB (79.58%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> currRow = new ArrayList<Integer>();
+        List<Integer> prevRow = new ArrayList<Integer>();
+        
+        prevRow.add(1);
+        
+        list.add(prevRow);
+        
+        for(int i = 0; i < numRows-1; i++){
+            
+            currRow.add(1);
+            
+            for(int k = 0; k < i; k++){
+                currRow.add(prevRow.get(k) + prevRow.get(k+1));
+            }
+            
+            currRow.add(1);
+            
+            list.add(currRow);
+            
+            prevRow = currRow;
+            
+            currRow = new ArrayList<Integer>();
+        }
+        
+        
+        return list;
+        
+    }
+    
+    public List<Integer> getRow(int rowIndex) {
+        return generate(rowIndex+1).get(rowIndex);
+    }
+}
